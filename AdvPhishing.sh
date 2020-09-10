4048577a296a1e1b6dbc845c67e2c482fd8c1db2
Dante <70626142+Surajp8055@users.noreply.github.com>
2020-09-10 20:04:07 +0530

    Add files via upload

---
+++ AdvPhishing.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+#this line is know as shebang
+#let's start
+figlet Dante Tool Maker | lolcat
+read -p "Enter your Name :> " NAME
+echo " Welcome $NAME " | lolcat
+git clone https://github.com/Ignitetch/AdvPhishing.git
+clear
+ echo " Thank You $NAME" | lolcat
+ figlet For Installing Tool | lolcat
